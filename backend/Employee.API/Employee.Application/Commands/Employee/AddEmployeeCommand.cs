a5cedadde19a923255e463b44eb95b39d67cf30a
Arghya-n <sakib.hb7@gmail.com>
2025-02-15 21:57:49 +0600

    Clean architecture folder structure along with employee and task endpoints added

---
+++ backend/Employee.API/Employee.Application/Commands/Employee/AddEmployeeCommand.cs
@@ -0,0 +1,17 @@
+﻿using Employee.Core.Entities;
+using Employee.Core.Interfaces;
+using MediatR;
+
+namespace Employee.Application.Commands.Employee
+{
+    public record AddEmployeeCommand(EmployeeEntity Employee) : IRequest<EmployeeEntity>;
+
+    public class AddEmployeeCommandHandler(IEmployeeRepository employeeRepository)
+        : IRequestHandler<AddEmployeeCommand, EmployeeEntity>
+    {
+        public async Task<EmployeeEntity> Handle(AddEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            return await employeeRepository.AddEmployee(request.Employee);
+        }
+    }
+}
