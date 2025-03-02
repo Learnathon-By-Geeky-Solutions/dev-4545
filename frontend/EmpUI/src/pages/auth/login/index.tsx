3e9d4e0b5e9ce428073be541d90a69d1113fb817
shohag1102 <saikathossain1102@gmail.com>
2025-03-02 14:05:27 +0600

    backend delete

--- frontend/EmpUI/src/pages/auth/login/index.tsx
+++ frontend/EmpUI/src/pages/auth/login/index.tsx
@@ -1,11 +1,11 @@
-import FormDescription from '@features/auth/components/form-description';
-
-import LoginForm from '@features/auth/login-form';
+import React from "react";
+import FormDescription from "@features/auth/components/form-description";
+import LoginForm from "@features/auth/login-form";
 
 const Login = () => {
   return (
     <>
-      <FormDescription formType={'login'} />
+      <FormDescription formType={"login"} />
       <div className="w-[360px]">
         <LoginForm />
       </div>
