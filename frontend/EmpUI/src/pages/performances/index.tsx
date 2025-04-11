8866f58219bf1719aeada368177986b5c73f9556
Md. Saikat Hossain Shohag <u1904088@student.cuet.ac.bd>
2025-04-11 11:47:14 +0600

    performance init

---
+++ frontend/EmpUI/src/pages/performances/index.tsx
@@ -0,0 +1,26 @@
+import { Space, Spin } from "antd";
+import PageContent from "@layouts/partials/page-content";
+import PageHeader from "@layouts/partials/page-header";
+import { usePerformances } from "@hooks/use-performances";
+
+const Performances = () => {
+  const { isLoading, data: performances } = usePerformances();
+
+  console.log(performances);
+
+  return (
+    <>
+      <PageHeader
+        title="Performances"
+        subTitle="Access and adjust your preferences conveniently on our Performances page"
+      />
+      <PageContent>
+        <Space direction="vertical" size="large" style={{ display: "flex" }}>
+          <Spin spinning={isLoading}>{/* performances table */}</Spin>
+        </Space>
+      </PageContent>
+    </>
+  );
+};
+
+export default Performances;
