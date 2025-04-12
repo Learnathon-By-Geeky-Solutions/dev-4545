bbaa482c012cb13f06727f847d74377eea938077
Arghya-n <sakib.hb7@gmail.com>
2025-04-12 22:12:49 +0600

    Production ready

--- backend/Employee.API/Employee.API/Program.cs
+++ backend/Employee.API/Employee.API/Program.cs
@@ -112,7 +112,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 
 {
 
