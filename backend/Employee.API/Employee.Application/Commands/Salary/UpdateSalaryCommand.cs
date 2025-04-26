cdcec4f535968d5b6f73b792b12bfeb9c0806978
Arghya-n <sakib.hb7@gmail.com>
2025-04-26 13:39:21 +0600

    SalaryHandler Unit-Tests Added

--- backend/Employee.API/Employee.Application/Commands/Salary/UpdateSalaryCommand.cs
+++ backend/Employee.API/Employee.Application/Commands/Salary/UpdateSalaryCommand.cs
@@ -9,11 +9,11 @@ using MediatR;
 
 namespace Employee.Application.Commands.Salary
 {
-    public record UpdateSalaryCommand(Guid EmployeeId,SalaryEntity SalaryEntity): IRequest<SalaryEntity>;
+    public record UpdateSalaryCommand(Guid EmployeeId,SalaryEntity SalaryEntity): IRequest<SalaryEntity?>;
     public class UpdateSalaryCommandHandler(ISalaryRepository salaryRepository)
-        : IRequestHandler<UpdateSalaryCommand, SalaryEntity>
+        : IRequestHandler<UpdateSalaryCommand, SalaryEntity?>
     {
-        public async Task<SalaryEntity> Handle(UpdateSalaryCommand request, CancellationToken cancellationToken)
+        public async Task<SalaryEntity?> Handle(UpdateSalaryCommand request, CancellationToken cancellationToken)
         {
             var result = await salaryRepository.UpdateSalary(request.EmployeeId, request.SalaryEntity);
             return result;
